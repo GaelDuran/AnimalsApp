6d9555b93b611dca85a7c25ca0f36ae71130ea84
GaelDuran <ldt76578@lasallebajio.edu.mx>
2025-05-08 22:44:26 -0600

    Implementacion del AnimalsServices

--- app/src/main/java/com/example/animalsapp/services/AnimalsService.kt
+++ app/src/main/java/com/example/animalsapp/services/AnimalsService.kt
@@ -1,2 +1,9 @@
 package com.example.animalsapp.services
 
+import com.example.animalsapp.models.Animals
+import retrofit2.http.GET
+
+interface AnimalsService {
+    @GET("animals")
+    suspend fun getAnimals(): List<Animals>
+}
