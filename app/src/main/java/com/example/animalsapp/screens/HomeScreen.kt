840fadfddedbfa3546e70c13392d98da1d100785
GaelDuran <ldt76578@lasallebajio.edu.mx>
2025-05-09 18:11:36 -0600

    Retrofit en HomeScreen

--- app/src/main/java/com/example/animalsapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/animalsapp/screens/HomeScreen.kt
@@ -1,26 +1,42 @@
 package com.example.animalsapp.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import android.util.Log
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.example.animalsapp.models.Animals
+import com.example.animalsapp.services.AnimalsService
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Composable
-fun HomeScreen(animals: List<Animals>, onAnimalClick: (Animals) -> Unit, navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF1B5E20))
-            .padding(16.dp)
-    ){}
+fun HomeScreen(innerPadding: PaddingValues) {
+    var animals by remember { mutableStateOf<Animals?>(null) }
+    val scope = rememberCoroutineScope()
+
+    val BASE_URL = "https://animals.juanfrausto.com/api/animals"
+
+    LaunchedEffect(true) {
+        scope.launch {
+            try {
+                val retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                val animalServices = retrofit.create(AnimalsService::class.java)
+                animals = animalServices.getAnimals()
+            } catch (e: Exception) {
+                Log.e("HomeScreen", "Error al obtener los animales", e)
+            }
+        }
+    }
 }
 
 @Preview
